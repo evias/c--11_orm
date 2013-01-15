b1f40f944040ba2121b89a8815ab568a64e23e0b
Greg Saive <greg@evias.be>
2013-01-15 17:03:20 +0100

    add use case for select, from & where structs for tests purpose

--- main.cpp
+++ main.cpp
@@ -9,10 +9,14 @@
 #include <tuple>
 #include <string>
 #include "table.hpp"
+#include "query.hpp"
 
 int main(int argc, char** argv)
 {
     using evias::dbo::table;
+    using evias::dbo::select;
+    using evias::dbo::from;
+    using evias::dbo::where;
 
     table::set_connection_data("localhost", "db_evias", "root", "");
 
@@ -28,6 +32,11 @@ int main(int argc, char** argv)
      */
 
     //t_nopkey.query<std::string>("select firstname from table_nopkey");
+
+    std::cout << t_nopkey.query_select(select{"firstname", "lastname"},
+                                       from{"table_nopkey"},
+                                       where{"email='gsaive@databay.de'"})
+              << std::endl;
 
     std::cout << "Hello, world!" << std::endl;
 
